0932dc61ddbd11ca331026ea8231124e80b8ed3b
Ayşe Oğuz <ouzayse123@gmail.com>
2021-01-20 03:29:15 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void CustomerAdded(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.LastName + " adlı, operatörü " + customer.Operator + " olan müşteri sisteme eklenmiştir.");
+            Console.WriteLine("----------------------------------------------------");
+        }
+
+        public void CustomerDelete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.LastName + " isimli müşterimiz sistemden silinmiştir.");
+        }
+
+        public void CustomerList(Customer[] customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("\nMüşteri Id: " + customer.Id + "\nMüşteri Adı: " + customer.Name + "\nMüşteri Soyadı: " + customer.LastName + "\nMüşteri Operatörü: " + customer.Operator);
+                Console.WriteLine("-------------------------------");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerManager customerManager = new CustomerManager();
+
+            Customer customer1 = new Customer();
+            customer1.Id = 12345;
+            customer1.Name = "Dilara";
+            customer1.LastName = "Yıldırım";
+            customer1.Operator = "Avea";
+            customerManager.CustomerAdded(customer1);
+
+            Customer customer2 = new Customer();
+            customer2.Id = 23456;
+            customer2.Name = "Ali";
+            customer2.LastName = "Şimşek";
+            customer2.Operator = "Vodafone";
+            customerManager.CustomerAdded(customer2);
+
+            Customer customer3 = new Customer();
+            customer3.Id = 34567;
+            customer3.Name = "Serkan";
+            customer3.LastName = "Doğu";
+            customer3.Operator = "Turkcell";
+            customerManager.CustomerAdded(customer3);
+
+            Customer customer4 = new Customer();
+            customer4.Id = 456789;
+            customer4.Name = "Aslı";
+            customer4.LastName = "Kara";
+            customer4.Operator = "BİM CELL";
+            customerManager.CustomerAdded(customer4);
+
+            Console.WriteLine("------------------------Tüm Müşteriler----------------------------");
+            Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4 };
+            customerManager.CustomerList(customers);
+            Console.WriteLine("----------------------------------------------------");
+
+            customerManager.CustomerDelete(customer2);
+        }
+    }
+}
